847ffa68c427bdf015d80040aad9d3bf25d7f865
Aiscalp <Aiscalp@gmail.com>
2022-05-22 19:11:33 +0300

    End of section 6

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -15,7 +15,7 @@ export class AccountService {
   constructor(private http: HttpClient) { }
 
   login(model: any) {
-    return this.http.post(this.baseUrl + 'account/login', model).pipe(
+    return this.http.post(this.baseUrl + 'users/login', model).pipe(
       map((response: User) => {
         const user = response;
         if (user) {
@@ -27,7 +27,7 @@ export class AccountService {
   }
   
   register(model: any) {
-    return this.http.post(this.baseUrl + 'account/register',model).pipe(
+    return this.http.post(this.baseUrl + 'users/register',model).pipe(
       map((user: User) => {
         if (user) {
           localStorage.setItem('user',JSON.stringify(user));
